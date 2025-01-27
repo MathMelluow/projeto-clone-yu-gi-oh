8447559c76ef202aec494d826c3610c6df5792b4
MathMelluow <89663749+MathMelluow@users.noreply.github.com>
2025-01-27 14:59:30 -0300

    Add files via upload

--- src/js/index.js
+++ src/js/index.js
@@ -3,31 +3,43 @@ const btnVoltar = document.getElementById("btn-voltar");
 const cartoes = document.querySelectorAll(".cartao");
 let cartaoAtual = 0;
 
+cartoes.forEach(cartao => {
+  cartao.addEventListener("click", function() {
+    const cartaVirada = cartao.querySelector(".carta-virada");
+
+    // virar o cartão
+    cartao.classList.toggle("virar");
+    // mostrar o fundo da carta
+    cartaVirada.classList.toggle("mostrar-fundo-carta");
+
+    const descricao = cartao.querySelector(".descricao");
+    descricao.classList.toggle("esconder");
+  });
+});
+
 btnAvancar.addEventListener("click", function () {
+  if (cartaoAtual === cartoes.length - 1) return;
 
-    if (cartaoAtual === cartoes.length - 1) return;
+  esconderCartaoSelecionado();
 
-    esconderCartaoSelecionado();
-
-    cartaoAtual++;
-    mostrarCartao(cartaoAtual);
+  cartaoAtual++;
+  mostrarCartao(cartaoAtual);
 });
 
 btnVoltar.addEventListener("click", function () {
+  if (cartaoAtual === 0) return;
 
-    if (cartaoAtual === 0) return;
+  esconderCartaoSelecionado();
 
-    esconderCartaoSelecionado();
+  cartaoAtual--;
+  mostrarCartao(cartaoAtual);
+});
 
-    cartaoAtual--;
-    mostrarCartao(cartaoAtual);
-})
-
-function mostrarCartao() {
-    cartoes[cartaoAtual].classList.add("selecionado");
+function mostrarCartao(cartaoAtual) {
+  cartoes[cartaoAtual].classList.add("selecionado");
 }
 
 function esconderCartaoSelecionado() {
-    const cartaoSelecionado = document.querySelector(".selecionado");
-    cartaoSelecionado.classList.remove("selecionado");
+  const cartaoSelecionado = document.querySelector(".selecionado");
+  cartaoSelecionado.classList.remove("selecionado");
 }
